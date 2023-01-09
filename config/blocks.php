5c679cd6f90cfa95784dcd9cfd057886f3af5106
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-01-09 16:46:12 +0100

    Modify define of blocks

--- config/blocks.php
+++ config/blocks.php
@@ -7,41 +7,17 @@ $config['gutenberg_blocks'] = [
       'name' => 'Welcome',
       'title' => 'Welcome,'
     ],
-    'Features' => [
-      'name' => 'Features',
-      'title' => 'Features,'
-    ],
     'Benefits' => [
       'name' => 'Benefits',
       'title' => 'Benefits,'
     ],
-    'Clients' => [
-      'name' => 'Clients',
-      'title' => 'Clients,'
+    'Quote_clients' => [
+      'name' => 'Quote_clients',
+      'title' => 'Quote_clients,'
     ],
-    'AboutUs' => [
-      'name' => 'AboutUs',
-      'title' => 'AboutUs,'
-    ],
-    'References' => [
-      'name' => 'References',
-      'title' => 'References,'
-    ],
-    'News' => [
-      'name' => 'News',
-      'title' => 'News,'
-    ],
-    'NewsDescription' => [
-      'name' => 'NewsDescription',
-      'title' => 'NewsDescription,'
-    ],
-    'Contact' => [
-      'name' => 'Contact',
-      'title' => 'Contact,'
-    ],
-    'Maps' => [
-      'name' => 'Maps',
-      'title' => 'Maps,'
+    'LearnMore' => [
+      'name' => 'LearnMore',
+      'title' => 'LearnMore,'
     ],
   ]
 ];
