795a5957936d7f223fa451cbb9c7ba7abfada12b
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-01-16 16:46:24 +0100

    Update blocks Benefits learnMore and Video And Create new blocks: FAQ, HelpCenter, Reasons

---
+++ src/library/blocks/FAQ/FAQ.block.render.php
@@ -0,0 +1,46 @@
+<?php
+
+function acf_block_render_callback_FAQ($block)
+{
+    $slug = str_replace('acf/', '', $block['name']);
+
+    $context = Timber::get_context();
+    $context['block'] = $block;
+    $context['fields'] = get_fields();
+    $current_category = $_GET['category_name'];
+    if(empty($current_category)){
+        $current_category = null;
+        $category_name = 'All';
+    }
+    else
+    {
+        $category_name = str_replace('-',' ', $current_category);
+        $category_name = strtoupper($category_name);
+    }
+
+    $args = array(
+        'post_type' => "faq",
+        'orderby' => 'menu_order',
+		'order' => 'DESC',
+        'category_name' => $current_category,
+        'post_status' => array('publish')
+        );
+        $args = new WP_Query( $args );
+        $args = $args->posts;
+        $counter = 0;
+        foreach ($args as $arg)
+        {
+            $title = get_the_title($arg->ID);
+            $excerpt = get_the_excerpt($arg->ID);
+            $image = get_the_post_thumbnail_url($arg->ID,'');
+            $url_post = get_the_permalink($arg->ID);
+            $table[$counter]=array('title_post' => $title, 'post_excerpt' => $excerpt, 'post_image' => $image, 'url_post' => $url_post);
+            $counter++;
+        }
+        $context['args'] = $table;
+        $crumbs[0] = array('link' => home_url(), 'text' => 'Home' );
+        $crumbs[1] = array('link' => '', 'text' => 'Learn');
+        $crumbs[2] = array('link' => '', 'text' => $category_name);
+        $context['crumbs'] = $crumbs;
+    Timber::render('./FAQ.block.view.twig', $context);
+}
