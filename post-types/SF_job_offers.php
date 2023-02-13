fd8f0177d36dd94e607354fa22aef4735044785d
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-02-13 16:53:39 +0100

    Create ne post type Job offers

---
+++ post-types/SF_job_offers.php
@@ -0,0 +1,49 @@
+<?php
+
+
+class SF_job_offers extends SF_abstract_post
+{
+  public static $name = 'Job offers';
+  public static $slug = 'joboffers';
+
+  public function createPost()
+  {
+    $labels = array(
+      'name' => _x(self::$name, 'tag'),
+      'singular_name' => _x(self::$name, 'tag'),
+      'add_new' => _x('Add job offers', 'tag'),
+      'add_new_item' => _x('Add job offers', 'tag'),
+      'edit_item' => _x('Edit job offers', 'tag'),
+      'new_item' => _x('New job offers', 'tag'),
+      'view_item' => _x('Show job offers', 'tag'),
+      'search_items' => _x('Search job offers', 'tag'),
+      'not_found' => _x('Not found', 'tag'),
+      'not_found_in_trash' => _x('No found in trash', 'tag'),
+      'parent_item_colon' => _x('Parent:', 'tag'),
+      'menu_name' => _x(self::$name, 'tag'),
+    );
+
+    $args = array(
+      'labels' => $labels,
+      'hierarchical' => false,
+      'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
+      'taxonomies'  => array( 'category' ),
+      'public' => true,
+      'show_ui' => true,
+      'show_in_menu' => true,
+      'menu_position' => 45,
+      'menu_icon' => 'dashicons-feedback',
+      'show_in_nav_menus' => true,
+      'publicly_queryable' => true,
+      'exclude_from_search' => true,
+      'has_archive' => true,
+      'query_var' => false,
+      'can_export' => true,
+      'capability_type' => 'post',
+      'show_in_rest' => true,
+    );
+
+    register_post_type(self::$slug, $args);
+  }
+}
+new SF_job_offers();
